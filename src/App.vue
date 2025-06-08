e1b71611c96a89a97d9d279b4326160187e941da
Leandro Huber <leandro.huber@hammerconsult.com.br>
2025-06-08 18:09:45 -0300

    Creating weather, wind and sea forecasts

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<v-app>
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col>
+          <Top></Top>
+        </v-col>
+      </v-row>
+      <v-row>
+        <div v-if="weatherData">
+        <v-col>
+            <v-card class="weather-card" elevation="2">
+              <Today :weatherData="weatherData" class="mr-8"></Today>
+              <v-divider></v-divider>
+              <Partial :weatherData="weatherData"></Partial>
+              <v-divider></v-divider>
+              <Week :weatherData="weatherData"></Week>
+            </v-card>
+        </v-col>
+      </div>
+      </v-row>
+      </v-container>  
+  </v-main>
+</v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import Today from './components/Today.vue';
+  import Partial from './components/Partial.vue';
+  import Week from './components/Week.vue';
+  import Top from './components/Top.vue'
+  import axios from 'axios'
+  import { onMounted } from 'vue';
+
+  const weatherData = ref(null);
+
+  onMounted(async () => {
+    try {
+      const responseToday = await axios.get('http://localhost:3000/forecast');
+      weatherData.value = responseToday.data.forecast;
+      
+      console.log(weatherData.value); 
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  });
+</script>
+
+<style scoped>
+  .weather-card {
+    border-radius: 12px;
+  }
+</style>
---
+++ src/components/Partial.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  const props = defineProps({
+    weatherData: Object
+  })
+
+  const weatherDetails = computed(() => [
+    { 
+      emoji: '🌊', 
+      label: 'Ondas', 
+      value: `${props.weatherData.waveHeight} m` 
+    },
+    { 
+      emoji: props.weatherData.waveDirectionIcon, 
+      label: 'Ondas', 
+      value: `${props.weatherData.waveDirection}` },
+    { 
+      emoji: '💨', 
+      label: 'Vento', 
+      value: `${props.weatherData.windSpeed} m/s` },
+    { 
+      emoji: props.weatherData.waveDirectionIcon, 
+      label: 'Vento', 
+      value: `${props.weatherData.waveDirection}` },
+    { 
+      emoji: '🌡️', 
+      label: 'Água', 
+      value: props.weatherData.currentTemp }
+  ]);
+</script>
+
+<template>
+  <v-card-text>
+    <div class="d-flex">
+      <div v-for="(detail, index) in weatherDetails" :key="index" class="text-center mr-10">
+          <span style="font-size: 28px">{{ detail.emoji }}</span>
+          <div class="text-caption">{{ detail.value }}</div>
+          <div class="text-caption text-grey">{{ detail.label }}</div>
+      </div>
+    </div>
+  </v-card-text>
+</template>
+
+
