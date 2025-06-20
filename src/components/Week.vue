e5c8c781b4accd989c0799d2c292bab3f1a36157
Leandro Huber <leandro.huber@hammerconsult.com.br>
2025-06-20 12:48:39 -0300

    Ajuste

--- src/components/Week.vue
+++ src/components/Week.vue
@@ -18,22 +18,20 @@
                 class="text-center forecast-day" 
                 :style="{ border: '1px solid', borderColor: day.color }
               ">
-                <div style="font-size: 15px !important" class="text-caption font-weight-smaller mb-1 mt-1">{{ day.weekDay }}</div>
-                  <v-divider></v-divider>
-                <div style="font-size: 10px !important" class="text-caption font-weight-smaller mb-1 mt-1">{{ day.date }}</div>
+                <div style="font-size: 15px !important" class="text-caption">{{ day.weekDay }}</div>
                 <v-divider></v-divider>
-                <div style="font-size: 10px !important"  class="text-caption font-weight-smaller mb-1 mt-1">{{ day.time }}</div>
+                <div style="font-size: 10px !important" class="text-caption mb-1 mt-1">{{ day.date }}</div>
+                <v-divider></v-divider>
+                <div style="font-size: 10px !important"  class="text-caption mb-1 mt-1">{{ day.time }}</div>
                 
                 <v-divider></v-divider>
-                <div style="font-size: 18px" class="mb-1">{{ day.condicao.icon }}</div>
+                <div style="font-size: 25px !important" class="mb-1">{{ day.condicao.icon }}</div>
 
-                <div style="font-size: 8px !important" class="text-caption font-weight-bold mb-2">
-                  {{ day.condicao.text.substring(0, 11) }}
+                <div style="font-size: 9px !important" class="text-caption font-weight-bold mb-2">
+                  {{ day.condicao.text.substring(0, 12) }}
                 </div>
                 
-                <v-divider></v-divider>
-                <div style="font-size: 18px" class="mb-1">{{ day.emoji }}</div>
-                <div class="text-caption font-weight-bold mb-2">
+                <div style="font-size: 21px !important" class="text-caption font-weight-bold mb-2">
                   <div v-if="day.currentTemp > 15">
                     <span class="text-red">{{ day.currentTemp }}°</span>
                   </div>
@@ -41,24 +39,46 @@
                     <span class="text-blue">{{ day.currentTemp }}°</span> 
                   </div>
                 </div>
+
                 <v-divider></v-divider>
-                <div class="d-flex flex-column">
-                  <div class="d-flex align-center mt-1">
-                    <v-icon small class="mr-1">mdi-weather-windy</v-icon>
-                    <span class="text-caption">{{ day.windSpeed }} m/s</span>
+
+                <div class="d-flex flex-column align-center">
+
+                  <!-- Bloco do Vento -->
+                  <div class="d-flex flex-column align-center">
+                    <div style="font-size: 18px !important" class="d-flex flex-column align-center mt-1">
+                      <v-icon big class="mb-1">mdi-weather-windy</v-icon>
+                    </div>
+                    <div class="d-flex justify-center mt-1">
+                      <span class="text-caption">{{ day.windDirection }}</span>
+                    </div>
+                    <div class="d-flex flex-column align-center mt-1">
+                      <span class="text-caption">{{ day.windSpeed }} m/s</span>
+                    </div>
+                    <div style="font-size: 28px !important" class="d-flex justify-center mt-1 mb-2">
+                      <span>{{ day.windDirectionIcon }}</span>
+                    </div>
                   </div>
-                  <div class="d-flex align-start mt-1">
-                    <span class="text-caption">{{ day.windDirectionIcon }}</span>
-                    <span class="text-caption">{{ day.windDirection }}</span>
-                  </div>
+                </div>
+
                   <v-divider></v-divider>
-                  <div class="d-flex align-start mt-1">
-                    <v-icon small class="mr-1">mdi-wave</v-icon>
-                    <span class="text-caption">{{ day.waveHeight }}m</span>
-                  </div>
-                  <div class="d-flex align-start">
-                    <span class="text-caption">{{ day.waveDirectionIcon }}</span>
-                    <span class="text-caption">{{ day.waveDirection }}</span>
+            
+                <div class="d-flex flex-column align-center">
+                  <!-- Bloco das Ondas -->
+                  <div class="d-flex flex-column align-center">
+                    <div style="font-size: 28px !important" class="d-flex flex-column align-center">
+                      <v-icon>mdi-wave</v-icon>
+                    </div>
+                    <div class="d-flex justify-center">
+                      <span class="text-caption">{{ day.waveDirection }}</span>
+                    </div>
+
+                    <div class="d-flex flex-column align-center mt-1">
+                      <span class="text-caption">{{ day.waveHeight }}m</span>
+                    </div>
+                    <div style="font-size: 28px !important" class="d-flex justify-center mt-1">
+                      <span>{{ day.waveDirectionIcon }}</span>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -75,10 +95,9 @@
   }
   .forecast-day {
     width: 82px;
-    padding: 4px;
-    border-radius: 8px;
-    transition: background-color 0.3s;
-    font-size: small;
+    padding: 8px;
+    border-radius: 10px;
+    transition: background-color 0.5s;
     border: 2px solid #333; 
     border-radius: 4px; 
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.2); 
